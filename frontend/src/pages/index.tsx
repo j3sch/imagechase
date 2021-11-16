ba4de943e74a3b2c74183913146da607ed1fcec9
Jens Schlegel <jens.b.schlegel@gmail.com>
2021-11-16 14:25:12 +0000

    postgres volume added and hot reload for nextjs implmented

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,3 @@
-import { useEffect, useState } from 'react';
-
 export default function Home(): JSX.Element {
-	const [response, setResponse] = useState({});
-
-	useEffect(() => {
-		fetch('http://localhost:5000/contributions').then((response) =>
-			setResponse(response),
-		);
-	}, []);
-
-	return <div>{response}</div>;
+	return <div>.dsadsaaaaaaaaaaaaaaa</div>;
 }
