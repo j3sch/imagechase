3adf9207c5a760619dec9e502653a9c4c55618ee
Jens Schlegel <jens.b.schlegel@gmail.com>
2021-11-27 15:15:58 +0000

    submission getServerside

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const api = dev ? '/api' : 'https://api-imagechase.herokuapp.com'
+export const api = dev
+  ? 'http://localhost:3000/api'
+  : 'https://api-imagechase.herokuapp.com'
