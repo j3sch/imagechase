1f077efcb811428e26a165712928298a9816c839
Jens Schlegel <jens.b.schlegel@gmail.com>
2021-12-11 01:16:08 +0000

    preview page profil, leaderboard, prices

--- frontend/pages/prices.js
+++ frontend/pages/prices.js
@@ -1,13 +1,13 @@
 import Alert from 'react-bootstrap/Alert'
 
-export default function Profile() {
+export default function Prices() {
   return (
     <div
       className="d-flex justify-content-center align-items-center"
       style={{ height: '60vh' }}
     >
       <Alert variant="primary" className="p-4">
-        <p className="m-0 fs-5">Profile is currently in development </p>
+        <p className="m-0 fs-5">Prices is currently in development </p>
         <p className="m-0 fs-5">
           and will be available in the future.<i class="bi bi-stars"></i>
         </p>
