c1b6c0e6311af4d85b012c9330cffebc430c29d8
Jens Schlegel <jens.b.schlegel@gmail.com>
2021-12-11 12:24:02 +0000

    small chances to this feature is available in future

--- frontend/pages/user/[id]/profile.js
+++ frontend/pages/user/[id]/profile.js
@@ -6,11 +6,10 @@ export default function Profile() {
       className="d-flex justify-content-center align-items-center"
       style={{ height: '60vh' }}
     >
-      <Alert variant="primary" className="p-4">
-        <p className="m-0 fs-5">Profile is currently in development </p>
-        <p className="m-0 fs-5">
-          and will be available in the future.<i className="bi bi-stars"></i>
-        </p>
+      <Alert variant="primary" className="p-4 fs-5">
+        The profile page is currently in development and will be available in
+        the future.
+        <i className="bi bi-stars"></i>
       </Alert>
     </div>
   )
